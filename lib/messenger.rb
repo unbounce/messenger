ef19253163d4fd42e0643fe41d38766fea046e9b
James Brennan <james@jamesbrennan.ca>
2013-10-15 12:35:27 -0700

    Copied library over from sqs-worker

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -1,4 +1,18 @@
 require 'messenger/version'
 
 module Messenger
+
+  class << self
+    attr_accessor :config
+  end
+
+  def self.configure
+    self.config ||= Configuration.new
+    yield(config)
+  end
+
+  class Configuration
+    attr_accessor :listener_type, :worker_type
+  end
+
 end
---
+++ lib/messenger/worker.rb
@@ -0,0 +1,27 @@
+
+module Messenger
+  class Worker
+
+    def initalizer
+      @default_worker = worker_for Messenger.config.worker_type
+    end
+
+    def self.work(message, type = nil)
+      self.new.work message, type
+    end
+
+    def work(message, type = nil)
+      worker = type.nil? @default_worker : worker_for type
+      worker.work message
+    end
+
+    private
+
+      def worker_for(type)
+        classified = type.to_s.capitalize
+        klass = Object.const_get('Messenger').const_get('Workers').const_get("#{classified}Worker")
+        klass.new
+      end
+
+  end
+end
