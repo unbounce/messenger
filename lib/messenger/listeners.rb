76a7a6b24372ff212d925fab7fb9b174c2dcad04
James Brennan <james@jamesbrennan.ca>
2013-10-18 12:01:44 -0700

    Raise an error if listener.work is assigned without a work method

--- lib/messenger/listeners.rb
+++ lib/messenger/listeners.rb
@@ -1,15 +1,22 @@
 class Messenger
   module Listeners
 
-    attr_accessor :worker
+    attr_reader :worker
+
+    def worker=(worker)
+      @worker = worker
+      ensure_valid_worker
+    end
 
     def listen
       raise NotImplementedError.new('Subclass must implement listen')
     end
 
-    def ensure_valid_worker
-      raise unless @worker.respond_to? :work
-    end
+    private
+
+      def ensure_valid_worker
+        raise NotImplementedError.new('Worker must have work method') unless @worker.respond_to? :work
+      end
 
   end
 end
