88c8b8e949670f68d149809f12a967fdd1101662
James Brennan <james@jamesbrennan.ca>
2013-10-21 12:26:41 -0700

    If constant is not found, require the listener or worker first

--- lib/messenger.rb
+++ lib/messenger.rb
@@ -44,13 +44,27 @@ class Messenger
 
     def listener_for(type)
       classified = type.to_s.camelize
-      klass = Object.const_get('Messenger').const_get('Listeners').const_get("#{classified}")
+
+      begin
+        klass = Object.const_get('Messenger').const_get('Listeners').const_get("#{classified}")
+      rescue NameError
+        require "listeners/#{type}"
+        klass = Object.const_get('Messenger').const_get('Listeners').const_get("#{classified}")
+      end
+
       klass.new
     end
 
     def worker_for(type)
       classified = type.to_s.camelize
-      klass = Object.const_get('Messenger').const_get('Workers').const_get("#{classified}")
+
+      begin
+        klass = Object.const_get('Messenger').const_get('Workers').const_get("#{classified}")
+      rescue NameError
+        require "workers/#{type}"
+        klass = Object.const_get('Messenger').const_get('Workers').const_get("#{classified}")
+      end
+
       klass.new
     end
 
